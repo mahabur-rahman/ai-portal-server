987a4c80b364cb510dc068363d4262c01e3141ef
mahabur-rahman <annur4395@gmail.com>
2025-10-13 14:32:34 +0600

    chore: testing with github actions

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello updated for deploying test ok!!!';
+    return 'Automatic deployment test using github actions and aws ec2!!!';
   }
 }
